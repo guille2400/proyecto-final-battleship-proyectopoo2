e0b66f25ba561404034261dfe7e1a7b3ebb4e4e6
JuanGA12 <juan.galvez@utec.edu.pe>
2019-11-26 15:30:18 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/proyecto_final_battleship_proyectopoo2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyecto_final_battleship_proyectopoo2.dir/main.cpp.o"
+  "proyecto_final_battleship_proyectopoo2.pdb"
+  "proyecto_final_battleship_proyectopoo2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyecto_final_battleship_proyectopoo2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
