99a2dfa6d2ce0204df32170270dbad8541ad2d70
JuanGA12 <juan.galvez@utec.edu.pe>
2019-11-26 17:30:00 -0500

    Boad - Nicolas

--- Board.h
+++ Board.h
@@ -9,19 +9,23 @@
 class Board{
 
 private:
+    int **tablero;
     int filas;
     int columnas;
 
-
 public:
-
+    Board();
     Board(int,int);
-    void Construir();
-    void clear();
+
+    void llenar();
+
+    void imprimir();
+
     ~Board();
 
 
-};
 
+    void Construir(int f, int c);
+};
 
 #endif //PROYECTO_FINAL_BATTLESHIP_PROYECTOPOO2_BOARD_H
